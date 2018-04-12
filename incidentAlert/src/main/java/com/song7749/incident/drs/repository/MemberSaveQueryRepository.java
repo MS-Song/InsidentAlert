7fc3940aba057d2357580fed5ba66653000fbb3b
song7749@gmail.com <song7749@gmail.com@local-dev>
2018-04-12 16:45:45 +0900

    dbclient download 저장소

--- incidentAlert/src/main/java/com/song7749/incident/drs/repository/MemberSaveQueryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.song7749.incident.drs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.song7749.incident.drs.domain.MemberSaveQuery;
-
-public interface MemberSaveQueryRepository   extends JpaRepository<MemberSaveQuery, Long> {
-
-}
--- incidentAlert/src/main/java/com/song7749/incident/drs/value/LogLoginFindDto.java
+++
@@ -1,9 +0,0 @@
-package com.song7749.incident.drs.value;
-
-import com.song7749.base.AbstractDto;
-
-public class LogLoginFindDto extends AbstractDto {
-
-	private static final long serialVersionUID = -7186733994702453066L;
-
-}
